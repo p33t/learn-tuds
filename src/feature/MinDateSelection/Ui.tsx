ebf19b76a57179770a61648ac82bfb7af062877d
pwl <pwl@localhost>
2025-01-31 12:52:48 -0800

    Include a type in the module declaration for UDS
    This slightly improves type safety (I think)

--- src/feature/MinDateSelection/Ui.tsx
+++ src/feature/MinDateSelection/Ui.tsx
@@ -1,6 +1,6 @@
 import {useAppDispatch, useAppSelector} from "../../app/hooks"
 import * as redux from "./redux"
-import {DatePicker} from "@telus-uds/components-web";
+import {DatePicker, DatePickerProps} from "@telus-uds/components-web";
 import {useState} from "react";
 
 /** Selects the API Key for calling TMK */
@@ -8,14 +8,22 @@ export const Ui = () => {
   const dispatch = useAppDispatch()
   const selected = useAppSelector(redux.selectSelected)
   const [date, setDate] = useState()
+  const dpProps: DatePickerProps = {
+    id: 'datePicker', // This is a required field, and it would be nice if this caused errors when commented out
+    label: 'Date Picker',
+    dateFormat: 'YYYY-MM-DD',
+    tooltip: "the tooltip",
+    date,
+    onChanged: setDate,
+    isDayDisabled: () => false
+  }
   return (
     <span>
 
     <label htmlFor="minDate">Min Date: </label>
     <input type="date" name="minDate" id="minDate"
            onChange={evt => dispatch(redux.selectDateMillis(evt.target.valueAsNumber))} value={selected}/>
-    <DatePicker id="datePicker" label="Date Picker" dateFormat="YYYY-MM-DD" date={date} 
-                onDateChange={setDate}/>
+    <DatePicker {...dpProps}/>
   </span>
   )
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,5 +6,7 @@ declare module '@telus-uds/theme-allium' {
 declare module "@telus-uds/components-web" {
     import * as lib from './telus-uds-components-web'
     const DatePicker: lib.DatePicker
+    type DatePickerProps = lib.DatePickerProps
+    // const DatePickerProps: lib.DatePickerProps
     const BaseProvider: lib.BaseProvider
 }
