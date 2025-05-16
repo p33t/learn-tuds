be8665d5a154ab655bece8107ffb100a2e7f7612
pwl <pwl@localhost>
2025-05-16 16:14:56 -0700

    Use <Link> in Sidebar (not Listbox href)

--- src/feature/Sidebar/Ui.tsx
+++ src/feature/Sidebar/Ui.tsx
@@ -1,13 +1,18 @@
-import {Listbox} from "@telus-uds/components-web";
 import * as Sidebar from "./index.ts";
+import {Link} from "react-router-dom";
 
 /** Sidebar for navigation */
 export const Ui = ({baseRoute}: Sidebar.Props) => (
     <div style={{width: "200px"}}>
-        <Listbox
-            items={[
-                {label: "Home", href: `${baseRoute}/`},
-                {label: "About", href: `${baseRoute}/about`}
-            ]}/>
+        <Link to={`${baseRoute}/`}>Home</Link><br/>
+        <Link to={`${baseRoute}/about`}>About</Link><br/>
+        
+        {/*Need to switch to LinkRouter technique to avoid page reloads but don't know how*/}
+        {/*import {Listbox} from "@telus-uds/components-web";*/}
+        {/*<Listbox*/}
+        {/*    items={[*/}
+        {/*        {label: "Home", href: `${baseRoute}/`},*/}
+        {/*        {label: "About", href: `${baseRoute}/about`}*/}
+        {/*    ]}/>*/}
     </div>
 )
