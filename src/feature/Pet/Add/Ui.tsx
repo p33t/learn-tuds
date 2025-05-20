f47f724de3ced4e02f17d1496c30274a8b299665
pwl <pwl@localhost>
2025-05-20 16:22:36 -0700

    Use <Formik> tag

--- src/feature/Pet/Add/Ui.tsx
+++ src/feature/Pet/Add/Ui.tsx
@@ -1,7 +1,7 @@
 // import * as PetStore from "../../../middleware/petstore-client";
 // import {useEffect} from "react";
 // import {useParams} from "react-router-dom";
-import {useFormik} from "formik";
+import {Formik} from "formik";
 import {PetStatus, PetStatusNames} from "../../../middleware/petstore-client";
 import {FlexGrid} from "@telus-uds/components-web";
 import * as Yup from 'yup';
@@ -36,62 +36,79 @@ export const Ui = () => {
     //     return errors;
     // };
     
-    const formik = useFormik<FormValues>({
-        initialValues: {
-            id: 0,
-            name: '',
-            category: '',
-            status: PetStatusNames[0]
-        },
-        // validate,
-        validationSchema: Yup.object({
-            name: Yup.string()
-                .max(15, 'Must be 15 characters or less')
-                .required('Required'),
-            category: Yup.string()
-                .max(20, 'Must be 20 characters or less')
-                .required('Required')
-        }),        
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        },
-    });
-    
-    return (
-        <form onSubmit={formik.handleSubmit}>
-            <FlexGrid>
-                <FlexGrid.Row>
-                    <FlexGrid.Col xs={3}>
-                        <label htmlFor="name">Name</label>
-                    </FlexGrid.Col>
-                    <FlexGrid.Col xs={6}>
-                        <input
-                            id="name"
-                            type="text"
-                            {...formik.getFieldProps('name')}
-                        />
-                        {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
-                    </FlexGrid.Col>
-                </FlexGrid.Row>
-                <FlexGrid.Row>
-                    <FlexGrid.Col xs={3}>
-                        <label htmlFor="category">Category</label>
-                    </FlexGrid.Col>
-                    <FlexGrid.Col xs={6}>
-                        <input
-                            id="category"
-                            type="text"
-                            {...formik.getFieldProps('category')}
-                        />
-                        {formik.touched.category && formik.errors.category ? <div>{formik.errors.category}</div> : null}
-                    </FlexGrid.Col>
-                </FlexGrid.Row>
-                <FlexGrid.Row>
-                    <FlexGrid.Col>
-                        <button type="submit">Submit</button>
-                    </FlexGrid.Col>
-                </FlexGrid.Row>
-            </FlexGrid>
-        </form>
-    )
+    // Before <Formik> tag
+    // const formik = useFormik<FormValues>({
+    //     initialValues: {
+    //         id: 0,
+    //         name: '',
+    //         category: '',
+    //         status: PetStatusNames[0]
+    //     },
+    //     // validate,
+    //     validationSchema: Yup.object({
+    //         name: Yup.string()
+    //             .max(15, 'Must be 15 characters or less')
+    //             .required('Required'),
+    //         category: Yup.string()
+    //             .max(20, 'Must be 20 characters or less')
+    //             .required('Required')
+    //     }),        
+    //     onSubmit: values => {
+    //         alert(JSON.stringify(values, null, 2));
+    //     },
+    // });
+
+    const initialValues: FormValues = { id: 0, name: '', category: '', status: PetStatusNames[0] };
+
+    return (<Formik initialValues={initialValues}
+                    validationSchema={Yup.object({
+                        name: Yup.string()
+                            .max(15, 'Must be 15 characters or less')
+                            .required('Required'),
+                        category: Yup.string()
+                            .max(20, 'Must be 20 characters or less')
+                            .required('Required')
+                    })}
+                    onSubmit={values => {
+                        alert(JSON.stringify(values, null, 2));
+                    }}>
+        {formik => (
+            <form onSubmit={formik.handleSubmit}>
+                <FlexGrid>
+                    <FlexGrid.Row>
+                        <FlexGrid.Col xs={3}>
+                            <label htmlFor="name">Name</label>
+                        </FlexGrid.Col>
+                        <FlexGrid.Col xs={6}>
+                            <input
+                                id="name"
+                                type="text"
+                                {...formik.getFieldProps('name')}
+                            />
+                            {formik.touched.name && formik.errors.name ? <div>{formik.errors.name}</div> : null}
+                        </FlexGrid.Col>
+                    </FlexGrid.Row>
+                    <FlexGrid.Row>
+                        <FlexGrid.Col xs={3}>
+                            <label htmlFor="category">Category</label>
+                        </FlexGrid.Col>
+                        <FlexGrid.Col xs={6}>
+                            <input
+                                id="category"
+                                type="text"
+                                {...formik.getFieldProps('category')}
+                            />
+                            {formik.touched.category && formik.errors.category ?
+                                <div>{formik.errors.category}</div> : null}
+                        </FlexGrid.Col>
+                    </FlexGrid.Row>
+                    <FlexGrid.Row>
+                        <FlexGrid.Col>
+                            <button type="submit">Submit</button>
+                        </FlexGrid.Col>
+                    </FlexGrid.Row>
+                </FlexGrid>
+            </form>
+        )}
+    </Formik>)
 }
