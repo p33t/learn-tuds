b1e1aa4ae394f2b4b468efd14eb1079630f10b1b
pwl <pwl@localhost>
2025-10-29 12:19:06 -0700

    Sort lines (types/index.d.ts)

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -5,17 +5,17 @@ declare module '@telus-uds/theme-allium' {
 
 declare module "@telus-uds/components-web" {
     import * as lib from './telus-uds-components-web'
-    const DatePicker: lib.DatePicker
-    type DatePickerProps = lib.DatePickerProps
     const BaseProvider : lib.BaseProvider
     const Box : lib.Box
+    const Button = lib.Button
+    const CheckBox = lib.Checkbox
+    const CheckboxGroup = lib.CheckboxGroup
+    const DatePicker: lib.DatePicker
+    const FlexGrid = lib.FlexGrid
+    const Listbox = lib.Listbox
     const Modal = lib.Modal
     const SideNav = lib.SideNav
     const TextInput = lib.TextInput
     const Typography = lib.Typography
-    const CheckBox = lib.Checkbox
-    const CheckboxGroup = lib.CheckboxGroup
-    const Button = lib.Button
-    const Listbox = lib.Listbox
-    const FlexGrid = lib.FlexGrid
+    type DatePickerProps = lib.DatePickerProps
 }
