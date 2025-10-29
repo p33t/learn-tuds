61a162067070587633c7c074a6a0c9c97b373760
pwl <pwl@localhost>
2025-10-29 13:00:09 -0700

    Fix: Add 'key' attribute (SideNav.Ui)

--- src/feature/Sidebar/Ui.tsx
+++ src/feature/Sidebar/Ui.tsx
@@ -6,8 +6,9 @@ export const Ui = () => {
     const navigate = useNavigate()
     
     return (<SideNav>
-        <SideNav.Item onPress={() => navigate('/')}>Home</SideNav.Item>
-        <SideNav.Item onPress={() => navigate('/pet/list')}>Pets</SideNav.Item>
-        <SideNav.Item onPress={() => navigate('/about')}>About</SideNav.Item>
+        <SideNav.Item key='root' onPress={() => navigate('/')}>Home</SideNav.Item>
+        <SideNav.Item key='pet/list' onPress={() => navigate('/pet/list')}>Pets</SideNav.Item>
+        <SideNav.Item key='imageUpload' onPress={() => navigate('/imageUpload')}>Image Upload</SideNav.Item>
+        <SideNav.Item key='about' onPress={() => navigate('/about')}>About</SideNav.Item>
     </SideNav>)
 }
